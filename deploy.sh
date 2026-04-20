923d8bb2db5e87bb69641e4db59af0a0fbbc427a
Hell_Boy <vk1997vinodkumar@gmail.com>
2026-04-20 18:39:56 +0530

    code clean

--- deploy.sh
+++ deploy.sh
@@ -3,22 +3,9 @@
 # Switch Docker to use Minikube's environment
 eval $(minikube docker-env)
 
-# Run Maven clean install to build the JAR
-echo "Running Maven clean install..."
-mvn clean install -DskipTests
-
-# Delete old Docker image if it exists
-#echo "Deleting old Docker image spring-demo:latest (if present)..."
-#docker rmi -f spring-demo:latest || true
-
-# Build the Spring Boot Docker image without using cache
-echo "Building new Docker image spring-demo:latest (no cache)..."
-#docker build --no-cache -t spring-demo:latest .
-docker build -t spring-demo:latest .
-
-# Deleting existing deployment if it exists
-echo "Deleting existing Kubernetes deployment (if it exists)..."
-kubectl delete -f ./k8s/k8s-spring-deployment.yaml --ignore-not-found
+# # Deleting existing deployment if it exists
+# echo "Deleting existing Kubernetes deployment (if it exists)..."
+# kubectl delete -f ./k8s/k8s-spring-deployment.yaml --ignore-not-found
 
 # Apply the Kubernetes deployment
 echo "Applying Kubernetes deployment..."
